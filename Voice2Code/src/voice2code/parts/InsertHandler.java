6d13d7cb6c8187b24c216dc6b67acae3682df203
Pragadeesh <cpragadeesh@gmail.com>
2020-11-05 18:11:10 -0500

    mvp done

--- Voice2Code/src/voice2code/parts/InsertHandler.java
+++ Voice2Code/src/voice2code/parts/InsertHandler.java
@@ -7,12 +7,16 @@ import org.eclipse.ui.*;
 import org.eclipse.ui.texteditor.*;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 
 public class InsertHandler {
 	public InsertHandler() {}
 	
-	public void insertText(String text, int offset) {
+	public void insertText(String text) {
 		Display.getDefault().syncExec(new Runnable() {
 			@Override
 			public void run() {
@@ -23,11 +27,17 @@ public class InsertHandler {
 				IDocumentProvider dp = editor.getDocumentProvider();
 				IDocument document = dp.getDocument(editor.getEditorInput());
 				
+				Control control = editor.getAdapter(Control.class);
+				StyledText styledText = (StyledText) control;
+				int offset = styledText.getCaretOffset();
+				
 				try {
-					document.replace(offset, 0, text + "\n");
+					document.replace(offset, 0, text);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				
+				styledText.setCaretOffset(offset + text.length() + 1);
 			}
 		});
 	}
