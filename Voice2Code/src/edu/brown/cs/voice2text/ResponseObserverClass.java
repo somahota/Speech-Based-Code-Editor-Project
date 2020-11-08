a0d05b29507524a359f88e182ec065def9b1f9df
Soma A. Hota <shnasabrainwave@gmail.com>
2020-11-08 06:06:47 -0500

    Update ResponseObserverClass.java
    added command "Down"

--- Voice2Code/src/edu/brown/cs/voice2text/ResponseObserverClass.java
+++ Voice2Code/src/edu/brown/cs/voice2text/ResponseObserverClass.java
@@ -5,10 +5,11 @@ import com.google.api.gax.rpc.StreamController;
 import com.google.cloud.speech.v1p1beta1.StreamingRecognizeResponse;
 
 import voice2code.parts.InsertHandler;
+import java.util.StringTokenizer;
 
 public class ResponseObserverClass implements ResponseObserver<StreamingRecognizeResponse>{
 	private InsertHandler insertHandler;
-	
+	//public String transcript;
 	public ResponseObserverClass() {
 	}
 	
@@ -30,8 +31,24 @@ public class ResponseObserverClass implements ResponseObserver<StreamingRecogniz
 		System.out.println("Transcript: " + transcript);
 		System.out.println("Confidence: " + ((StreamingRecognizeResponse) response).getResults(0).getAlternatives(0).getConfidence());
 		System.out.println("Final: " + ((StreamingRecognizeResponse) response).getResults(0).getIsFinal());
+		StringTokenizer st = new StringTokenizer(transcript);
+		while (st.hasMoreTokens()) {  
+			String token = st.nextToken();
+			System.out.println("Tokenize:" + token );
+		}
 		insertHandler.insertText(transcript);
 	}
+	
+ 	public String tokenize(String token)
+	{
+ 		StringTokenizer key = new StringTokenizer(token);
+ 		String command ="";
+ 		while(key.hasMoreTokens())
+ 		{
+		   command = key.nextToken();
+ 		}
+		return command;	
+		}
 
 
 	@Override
